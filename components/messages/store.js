c302bd9fd21f04438ada36872c5d8d05239bcb92
Alon <alinrealin@gmail.com>
2019-12-30 08:38:39 -0700

    conexion con mongoDB atlas

--- components/messages/store.js
+++ components/messages/store.js
@@ -1,8 +1,11 @@
 const db = require('mongoose');
 db.Promise = global.Promise;
 db.connect("mongodb+srv://alinrealin:210220123@cluster0-tr0ss.mongodb.net/test",{
-    useNewUrlParser:true
+    useNewUrlParser:true,
+    useUnifiedTopology: true
 });
+
+console.log("[db] conectado con exito a mongoDB atlas");
 
 function addMessage(message){
     list.push(message);
